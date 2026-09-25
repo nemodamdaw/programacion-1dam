0e976952ab6ac2a5b60158bbef2b45805209492e
paco <paco@192.168.56.1>
2026-09-25 13:59:43 +0200

    mi segundo commit

--- UD4EjerciciosParte3/src/ejercico1/Ejercicio1.java
+++ UD4EjerciciosParte3/src/ejercico1/Ejercicio1.java
@@ -5,6 +5,8 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		System.out.println("Este es el elercicio1");
 		System.out.println("de la ud4 parte3");
+		Integer x=15;
+		System.out.println(x);
 
 	}
 
