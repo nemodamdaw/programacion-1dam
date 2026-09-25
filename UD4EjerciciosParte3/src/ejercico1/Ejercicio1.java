348e35119fdbf95eaceb7e58294f69bc9afb2e35
paco <paco@192.168.56.1>
2026-09-25 13:55:02 +0200

    mi primer commit

---
+++ UD4EjerciciosParte3/src/ejercico1/Ejercicio1.java
@@ -0,0 +1,11 @@
+package ejercico1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Este es el elercicio1");
+		System.out.println("de la ud4 parte3");
+
+	}
+
+}
